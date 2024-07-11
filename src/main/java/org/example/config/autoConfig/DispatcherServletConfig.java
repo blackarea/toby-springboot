37e8f603c79375e780bbe06f5313a96415c11dd5
hsl24 <hslee@skyware.co.kr>
2024-07-11 16:54:36 +0900

    섹션 6 - 자동 구성 애노테이션 적용

--- src/main/java/org/example/config/autoConfig/DispatcherServletConfig.java
+++ src/main/java/org/example/config/autoConfig/DispatcherServletConfig.java
@@ -1,8 +1,10 @@
 package org.example.config.autoConfig;
 
+import org.example.config.MyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@MyAutoConfiguration
 public class DispatcherServletConfig {
 
     @Bean
